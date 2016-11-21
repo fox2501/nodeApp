f4cb4ea9d91857ec3fcc39c69c72614a68842d29
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-11-21 15:10:36 +0000

    ertyu

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 
-http.createServer(funtion(request, response) {
+http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type' : 'text/plain'});
     response.end('Hello World\n');
 }).listen(port);
